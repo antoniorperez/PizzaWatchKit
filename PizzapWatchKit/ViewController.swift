ca31abcbe4ce71b7c4d8369047ba3bd7b00fb527
antoniorperez <antoniorperez85@gmail.com>
2016-02-18 22:30:39 +0100

    Initial Commit

---
+++ PizzapWatchKit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PizzapWatchKit
+//
+//  Created by Antonio Rodríguez on 18/2/16.
+//  Copyright © 2016 Antonio Rodríguez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
